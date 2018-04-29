f76d69dd4c48de1512640547a64044612d1df8eb
Fredrik <fredrik.christenson@gmail.com>
2018-04-29 10:53:02 +0200

    rm

--- src/jspp/main.jspp.js
+++
@@ -1,3 +0,0 @@
-// Compiled with JS++ v.0.8.1
-
-!function(){var e=function(){if(typeof console!=="undefined"&&typeof console.log==="function"){return(console.log);}else if(typeof print==="function"){return(print);}else {return((function(){}));}}();var f=function(){if(typeof console!=="undefined"&&typeof console.log==="function"){return(console);}else {return(void 0);}}();!function(){e.apply(f,["foo"]);e.apply(f,["--------------------------"]);}();}();
